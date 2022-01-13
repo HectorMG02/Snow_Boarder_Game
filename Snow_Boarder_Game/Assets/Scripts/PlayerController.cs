946b2f00a819f8ac34439076d19de99a164a0ce0
hector.matias@equipoiriego.es <hector.matias@equipoiriego.es>
2022-01-13 20:39:23 +0100

    particulas al deslizarnos sobre la nieve

--- Snow_Boarder_Game/Assets/Scripts/PlayerController.cs
+++ Snow_Boarder_Game/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 3f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+     
+    
+    Rigidbody2D rb2d;
+    private SurfaceEffector2D surfaceEffector2D;
     
     private void Start()
     { 
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>(); 
     }
 
     private void Update()
     {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        { 
+            surfaceEffector2D.speed = boostSpeed;
+        }else
+        { 
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb2d.AddTorque(torqueAmount); // torque es una fuerza para rotar el objeto, en este caso lo rotamos hacia la izquierda
+            rb2d.AddTorque(
+                torqueAmount); // torque es una fuerza para rotar el objeto, en este caso lo rotamos hacia la izquierda
         }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
